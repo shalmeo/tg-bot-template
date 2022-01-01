108c79ed33a1b1af69b3dc3a2e721cbd50607df0
Sha1m <algaevshamil8@gmail.com>
2022-01-01 18:11:53 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+
+from tgbot.config import load_config
+from tgbot.misc.set_bot_commands import set_dafault_commands
+from tgbot import middlewares, filters, handlers
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger.info("Starting bot")
+    config = load_config(".env")
+
+    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(bot, storage=storage)
+
+    bot['config'] = config
+
+    middlewares.setup(dp)
+    filters.setup(dp)
+    handlers.setup(dp)
+
+    await set_dafault_commands()
+
+    # start
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Bot stopped!")
