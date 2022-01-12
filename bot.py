8ee29d7624d3a9944da950c91e60f741e36aefab
Sha1m <algaevshamil8@gmail.com>
2022-01-12 20:21:16 +0300

    main

--- bot.py
+++ bot.py
@@ -34,6 +34,7 @@ async def main():
 
     # start
     try:
+        await dp.skip_updates()
         await dp.start_polling()
     finally:
         await dp.storage.close()
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+from tgbot.misc.throttling import rate_limit
+
 
 @rate_limit(5, key='start')
 async def user_start(message: Message):
