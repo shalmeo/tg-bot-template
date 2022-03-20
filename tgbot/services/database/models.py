dc6cc7fa9a56cec0564722d7aa8b122ae6658fc4
Sha1m <algaevshamil8@gmail.com>
2022-03-20 16:23:08 +0300

    main

--- tgbot/services/database/models.py
+++ tgbot/services/database/models.py
@@ -8,11 +8,3 @@ class User(TimedBaseModel):
     
     id = Column(BigInteger, primary_key=True)
     full_name = Column(String(255), nullable=False)
-    status = Column(Boolean, default=False, nullable=False)
-
-
-class Trash(TimedBaseModel):
-    __tablename__ = 'Trash'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
